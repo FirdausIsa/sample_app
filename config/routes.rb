22c9f4b6b4b1d767c7d62231c52fe68202599374
FirdausIsa <firdausisa2525@gmail.com>
2021-08-20 15:54:41 +0800

    chapter_3

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     get 'home/index'
     resources :posts
-    root 'static_pages#home'
-    get 'static_pages/help'
+    root 'static_pages#home', as: "home" 
+    get 'static_pages/help', as: "help"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
